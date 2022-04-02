6349120a3573957ecd626e0af273c7c712595108
OtabekUmarov <umarovotabek0220@gmail.com>
2022-04-02 06:34:48 +0500

    first

---
+++ router/page.js
@@ -0,0 +1,62 @@
+const {
+    Router
+} = require('express')
+const router = Router()
+
+
+
+
+
+router.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Home',
+        layout: "site",
+        success: req.flash('success'),
+        error: req.flash('error'),
+        isHome: true
+    })
+})
+
+router.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About',
+        layout: "site",
+        isAbout: true
+    })
+})
+
+router.get('/class', (req, res) => {
+    res.render('class', {
+        title: 'Classes',
+        layout: "site",
+        isClass: true
+    })
+})
+
+router.get('/team', (req, res) => {
+    res.render('team', {
+        title: 'Teachers',
+        layout: "site",
+        isTeam: true
+    })
+})
+router.get('/gallery', (req, res) => {
+    res.render('gallery', {
+        title: 'Gallery',
+        layout: "site",
+        isGallery: true
+    })
+})
+router.get('/contact', (req, res) => {
+    res.render('contact', {
+        title: 'Contact',
+        layout: "site",
+        isContact: true
+    })
+})
+
+
+
+
+
+module.exports = router
